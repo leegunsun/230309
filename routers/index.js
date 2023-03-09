feb4d9d33a0011d304f70c521a592972ada67819
leegunsun <talkcjstk@naver.com>
2023-03-09 15:35:54 +0900

    test2

--- routers/index.js
+++ routers/index.js
@@ -2,11 +2,14 @@ const express = require("express");
 const router = express.Router();
 const Post = require("../schemas/posts.js");
 
+//모든 포스트를 검색합니다
 router.get("/", async (req, res, next) => {
   const allPost = await Post.find({});
   res.json({ allPost: allPost });
 });
 
+// 포스트를 작성합니다 title, content, postId가 있으며 postId는
+// 나중에 작성을 수정하거나 삭제 할때 쓰입니다.
 router.post("/", async (req, res, next) => {
   const { title, content, postId } = req.body;
 
@@ -17,6 +20,8 @@ router.post("/", async (req, res, next) => {
   res.send(`title : ${title}와 content : ${content}요청을 보냈습니다.`);
 });
 
+// 포스트를 수정합니다. postId를 params로 받아서 특정 Post를 찾고
+// 그 내용을 수정합니다.
 router.put("/:postId", async (req, res, next) => {
   const { postId } = req.params;
   const { title, content } = req.body;
@@ -31,6 +36,8 @@ router.put("/:postId", async (req, res, next) => {
   res.json({ findPost: findPost });
 });
 
+// 포스트를 수정합니다. postId를 params로 받아서 특정 Post를 찾고
+// 그 내용을 삭제합니다
 router.delete("/:postId", async (req, res, next) => {
   const { postId } = req.params;
   const findPost = await Post.findOne({ postId });
