4c5a7da259bc019b72d6d23390a8e61a937a6087
leegunsun <talkcjstk@naver.com>
2023-04-12 15:20:18 +0900

    nginx 테스트

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
-const exepress = require("express");
-const connect = require("./schemas");
-const app = exepress();
-const port = 3000;
+const express = require("express");
+const app = express();
+const cors = require("cors");
 
-const indexRouter = require("./routers");
+const PORT = 3000;
 
-app.use(exepress.json());
-connect();
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+    optionsSuccessStatus: 200,
+    exposedHeaders: ["Authorization"],
+  })
+);
 
-app.use("/", indexRouter);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.listen(port, () => {
-  console.log(port, "포트로 서버가 열렸어요!");
+app.use("/", (req, res) => {
+  return res.json({ "암아허;ㅣ나엏;ㅣ": "일반 / 입니다" });
+});
+
+app.use("/api", (req, res) => {
+  return res.json({ "암아허;ㅣ나엏;ㅣ": "ㅁ나허;ㅁ니ㅏ허;ㄴ미" });
+});
+
+app.listen(PORT, () => {
+  console.log(PORT, "포트로 서버가 열렸어요!");
 });
