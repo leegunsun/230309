e0e17b12c8e9de876a0f1e5fee20d5f98561be12
leegunsun <talkcjstk@naver.com>
2023-04-14 21:12:40 +0900

    굿

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use((req, res, next) => {
-  return res.json({ 제발: "수정을 했습니다. 두번째" });
+  return res.json({ 제발: "지금도 성공하고 있어" });
 });
 
 app.listen(PORT, () => {
