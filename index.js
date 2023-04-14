6ed4f5430cf51831ba841d83833163799dd86729
leegunsun <talkcjstk@naver.com>
2023-04-14 15:33:35 +0900

    이이이양야야

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use((req, res, next) => {
-  return res.json({ 제발: "성공ㅎ재gsdgsd" });
+  return res.json({ 제발: "성공했다 배포" });
 });
 
 app.listen(PORT, () => {
