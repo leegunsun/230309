b389679d3e2085a6181ad7f73849087ad34f28e7
leegunsun <talkcjstk@naver.com>
2023-04-17 22:19:10 +0900

    dd

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -6,6 +6,8 @@ const { Users } = require("../models");
 module.exports = async (req, res, next) => {
   const authorization = req.headers.authorization;
 
+  console.log("authorization ㄴ어ㅏ휜아훙니ㅏㅎ", authorization);
+
   const [authType, authToken] = (authorization ?? "").split(" ");
 
   if (authType !== "Bearer" || !authToken) {
@@ -13,6 +15,9 @@ module.exports = async (req, res, next) => {
       errorMessage: "로그인 후에 이용할 수 있는 기능입니다.",
     });
   }
+
+  console.log("authType ㄴ어ㅏ휜아ㄱ훙니ㅏㅎ", authToken);
+  console.log("authToken ㄴ어ㅏ휜아훙ㅇ니ㅏㅎ", authToken);
 
   try {
     const { email } = jwt.verify(authToken, process.env.SECRET_KEY);
